16781c371681eea8953d0b512135b3e9534be99a
SUNDAR <aaransoftware@gmail.com>
2025-04-05 10:56:53 +0530

    #20 revert for rajesh

--- aaran/BMS/Billing/Master/Providers/MasterServiceProvider.php
+++ aaran/BMS/Billing/Master/Providers/MasterServiceProvider.php
@@ -22,36 +22,22 @@ class MasterServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $this->loadMigrations();
-
         Livewire::component('master::company-list', CompanyList::class);
         Livewire::component('master::contact-list', ContactList::class);
-//        Livewire::component('company.upsert', Company\Upsert::class);
-//
-//        Livewire::component('contact.index', Contact\Index::class);
         Livewire::component('master::contact.upsert', ContactUpsert::class);
-//
 //
 //        Livewire::component('aaran.master.contact.lookup.contact-model', ContactModel::class);
 //        Livewire::component('aaran.master.order.lookup.order-model', OrderModel::class);
 //        Livewire::component('aaran.master.style.lookup.style-model', StyleModel::class);
 //        Livewire::component('aaran.master.product.lookup.product-model', ProductModel::class);
 //
-//
 //        Livewire::component('product.index', Product\Index::class);
 //        Livewire::component('orders.index', Orders\Index::class);
 //        Livewire::component('style.index', Style\Index::class);
-
-
     }
 
     protected function loadViews(): void
     {
         $this->loadViewsFrom(__DIR__ . '/../Livewire/Views', $this->moduleNameLower);
-    }
-
-    protected function loadMigrations(): void
-    {
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
     }
 }
