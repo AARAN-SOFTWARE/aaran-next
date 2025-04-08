fee476e38ddf3db0ccd2b7c4a9cd3bf1c1a625ff
SUNDAR <aaransoftware@gmail.com>
2025-04-08 18:13:12 +0530

    #30 working on Entries

---
+++ aaran/BMS/Billing/Entries/Livewire/Class/SalesList.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Aaran\BMS\Billing\Entries\Livewire\Class;
+
+use Aaran\Assets\Traits\ComponentStateTrait;
+use Aaran\Assets\Traits\TenantAwareTrait;
+use Aaran\BMS\Billing\Entries\Models\Sale;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+
+class SalesList extends Component
+{
+    use ComponentStateTrait, TenantAwareTrait;
+
+    #region[create]
+    public function create(): void
+    {
+        ini_set('max_execution_time', 3600);
+        $this->redirect(route('sales.upsert', ['0']));
+    }
+    #endregion
+
+    #region[getObj]
+    public function getObj($id)
+    {
+        if ($id) {
+            $obj = Sale::find($id);
+            $this->common->vid = $obj->id;
+            return $obj;
+        }
+        return null;
+    }
+    #endregion
+
+    #region[trashData]
+    public function trashData(): void
+    {
+        $obj = $this->getObj($this->common->vid);
+        DB::table('saleitems')->where('sale_id', '=', $this->common->vid)->delete();
+        $obj->delete();
+        $this->showDeleteModal = false;
+        $message = "Deleted Successfully";
+        $this->dispatch('notify', ...['type' => 'success', 'content' => $message]);
+    }
+    #endregon
+
+    #region[print]
+    public function print($id): void
+    {
+        $this->redirect(route('sales.print', [$id]));
+    }
+    #endregion
+
+
+    #region[getList]
+    public function getList()
+    {
+        $this->sortField = 'invoice_no';
+
+        return Sale::on($this->getTenantConnection())
+            ->active($this->activeRecord)
+            ->when($this->searches, fn($query) => $query->searchByName($this->searches))
+            ->orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc')
+            ->paginate($this->perPage);
+    }
+    #endregion
+
+    #region[render]
+    public function render()
+    {
+        return view('entries::Sales.index')->with([
+            'list' => $this->getList()
+        ]);
+    }
+    #endregion
+}
---
+++ aaran/BMS/Billing/Entries/Providers/EntriesServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Aaran\BMS\Billing\Entries\Providers;
+
+use Aaran\Entries\Providers\EntriesRouteProvider;
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+
+use Aaran\BMS\Billing\Entries\Livewire\Class;
+
+class EntriesServiceProvider extends ServiceProvider
+{
+    protected string $moduleName = 'Entries';
+    protected string $moduleNameLower = 'entries';
+
+    public function register(): void
+    {
+        $this->app->register(EntriesRouteProvider::class);
+
+//        $this->app->bind(SalesInvoiceController::class, function ($app) {
+//            return new SalesInvoiceController();
+//        });
+
+        $this->loadViews();
+    }
+
+    public function boot(): void
+    {
+
+        Livewire::component('entries::sales-list', Class\SalesList::class);
+        Livewire::component('entries::sales-upsert', Class\SalesUpsert::class);
+
+//        Livewire::component('sales.eway-bill', Sales\EwayBill::class);
+//        Livewire::component('sales.einvoice', Sales\Einvoice::class);
+
+//        Livewire::component('purchase.index', Purchase\Index::class);
+//        Livewire::component('purchase.upsert', Purchase\Upsert::class);
+
+//        Livewire::component('payment.index', Payment\Index::class);
+//
+//        Livewire::component('export-sales.index', ExportSales\Index::class);
+//        Livewire::component('export-sales.upsert', ExportSales\Upsert::class);
+//        Livewire::component('export-sales.packing-list', ExportSales\PackingList::class);
+//
+    }
+
+    protected function loadConfigs(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config.php', $this->moduleNameLower);
+    }
+
+    protected function loadViews(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../Livewire/Class', $this->moduleNameLower);
+    }
+
+}
