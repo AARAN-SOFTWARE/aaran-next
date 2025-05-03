b7008bbd8c6dcbf09a7b36fb37053a707da95cce
SUNDAR <aaransoftware@gmail.com>
2025-05-03 13:41:39 +0530

    #121 working on frappe

--- aaran/ExternalPartners/Frappe/ErpNext/Livewire/Class/StockList.php
+++ aaran/ExternalPartners/Frappe/ErpNext/Livewire/Class/StockList.php
@@ -2,73 +2,50 @@
 
 namespace Aaran\ExternalPartners\Frappe\ErpNext\Livewire\Class;
 
-use Aaran\Assets\Traits\ComponentStateTrait;
-use Aaran\Assets\Traits\TenantAwareTrait;
 use Aaran\ExternalPartners\Frappe\ErpNext\Services\ErpNextService;
 use Exception;
 use Livewire\Component;
 
 class StockList extends Component
 {
-    use ComponentStateTrait, TenantAwareTrait;
+    public $selected = 'Wireless Mouse';
+    public $stockData = [];
 
-    protected $erpNextService;
+    protected ErpNextService $erpNextService;
 
-    // Dependency Injection of ErpNextService
-    public function __construct()
+    public function mount()
+    {
+
+        $this->getStockBalanceReport();
+    }
+
+    public function updatedSelected()
+    {
+        $this->getStockBalanceReport();  // Trigger report fetch on item change
+    }
+
+
+    public function getStockBalanceReport()
     {
         $this->erpNextService = new ErpNextService();
-    }
 
-    public function getList()
-    {
-        try {
-            $filters = [
-                'fields' => '["name", "item_code", "item_name", "stock_uom"]'
-            ];
-
-            $url = $this->erpNextService->baseUrl . "/api/resource/Item";
-
-
-            $response = $this->erpNextService->client()->get($url, $filters);
-
-
-            return $this->erpNextService->handleResponse($response);
-        } catch (Exception $e) {
-            return $this->erpNextService->handleError($e);
-        }
-    }
-
-    public $selectedItemGroup = 'Wireless Mouse';
-
-    public function updatedSelectedItemGroup(): void
-    {
-        $this->getStockBalanceReport($this->selectedItemGroup);
-    }
-
-    public function getStockBalanceReport($itemGroup = 'Wireless Mouse')
-    {
         try {
             $url = $this->erpNextService->baseUrl . "/api/method/frappe.desk.query_report.run";
 
             $response = $this->erpNextService->client()->get($url, [
                 'report_name' => 'Stock Balance',
-                'ignore_prepared_report' => 'True', // safer than "True"
+                'ignore_prepared_report' => 'True',
                 'filters' => json_encode([
-                    'item_group' => $itemGroup,
-                    'to_date' => '2025-01-03'
+                    'item_group' => $this->selected,
+                    'to_date' => '2025-01-03',
                 ])
             ]);
 
-            return $this->erpNextService->handleResponse($response);
+            $this->stockData = $this->erpNextService->handleResponse($response)['message']['result'] ?? [];
         } catch (Exception $e) {
-            return $this->erpNextService->handleError($e);
+            $this->stockData = [];
+            report($e); // optional: log the error
         }
-    }
-    public function loadStockReport($itemGroup)
-    {
-        $response = app(YourService::class)->getStockBalanceReport($itemGroup);
-        $this->stockData = $response['message']['result'] ?? [];
     }
 
     public function render()
--- aaran/ExternalPartners/Frappe/ErpNext/Livewire/Views/stock-list.blade.php
+++ aaran/ExternalPartners/Frappe/ErpNext/Livewire/Views/stock-list.blade.php
@@ -1,20 +1,22 @@
 <div>
     <x-slot name="header">Stock List</x-slot>
+
     <x-Ui::forms.m-panel>
 
         <x-Ui::loadings.loading/>
 
         <x-Ui::alerts.notification/>
 
-        <!-- Top Controls --------------------------------------------------------------------------------------------->
-        <x-Ui::forms.top-controls :show-filters="$showFilters"/>
+        <!-- Select Dropdown -->
+        <select wire:model="selected">
+            <option value="Wireless Mouse">Wireless Mouse</option>
+            <option value="Keyboard">Keyboards</option>
+        </select>
 
-        <!-- Table Caption -------------------------------------------------------------------------------------------->
-        <x-Ui::table.caption :caption="'City'">
-            {{--            {{$list->count()}}--}}
-        </x-Ui::table.caption>
-
-        <!-- Table Data ----------------------------------------------------------------------------------------------->
+        <!-- Search Button -->
+        <button class="px-3 py-2 bg-green-400" wire:click="getStockBalanceReport">
+            Search
+        </button>
 
         <x-Ui::table.form>
             <x-slot:table_header>
@@ -32,40 +34,33 @@
 
             <x-slot:table_body>
 
-                @if($stockData)
-
-                    @foreach ($list['message']['result'] as $row)
-                        <x-Ui::table.row>
-
-                            @if (isset($row['item_code']))
+                @if (!empty($stockData))
+                    @foreach ($stockData as $row)
+                        @if (!empty($row['item_code']))
+                            <x-Ui::table.row>
                                 <x-Ui::table.cell-text>{{ $loop->iteration }}</x-Ui::table.cell-text>
                                 <x-Ui::table.cell-text>{{ $row['item_code'] }}</x-Ui::table.cell-text>
                                 <x-Ui::table.cell-text left>{{ $row['item_name'] ?? '' }}</x-Ui::table.cell-text>
                                 <x-Ui::table.cell-text>{{ $row['warehouse'] ?? '' }}</x-Ui::table.cell-text>
                                 <x-Ui::table.cell-text>{{ $row['item_group'] ?? '' }}</x-Ui::table.cell-text>
-
                                 <x-Ui::table.cell-text>{{ $row['opening_qty'] ?? 0 }}</x-Ui::table.cell-text>
                                 <x-Ui::table.cell-text right>{{ $row['opening_val'] ?? 0 }}</x-Ui::table.cell-text>
-
                                 <x-Ui::table.cell-text>{{ $row['bal_qty'] ?? 0 }}</x-Ui::table.cell-text>
                                 <x-Ui::table.cell-text right>{{ $row['bal_val'] ?? 0 }}</x-Ui::table.cell-text>
                                 <x-Ui::table.cell-text right>{{ $row['val_rate'] ?? 0 }}</x-Ui::table.cell-text>
-                            @endif
-                        </x-Ui::table.row>
+                            </x-Ui::table.row>
+                        @endif
                     @endforeach
-
+                @else
+                    <x-Ui::table.row>
+                        <x-Ui::table.cell-text colspan="10" class="text-center text-gray-500">
+                            No stock data available.
+                        </x-Ui::table.cell-text>
+                    </x-Ui::table.row>
                 @endif
+
             </x-slot:table_body>
         </x-Ui::table.form>
-
-        {{--        <div class="pt-5">{{ $list->links() }}</div>--}}
-
-        <!-- Create/ Edit Popup --------------------------------------------------------------------------------------->
-
-        <x-Ui::forms.create :id="$vid">
-            <x-Ui::input.floating wire:model="vname" label="City Name"/>
-            <x-Ui::input.error-text wire:model="vname"/>
-        </x-Ui::forms.create>
 
     </x-Ui::forms.m-panel>
 </div>
